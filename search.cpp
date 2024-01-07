cdc8331d46e30b2963294c6dd3f40cb5e3ef9cf2
Vicente Pareja <vicente.pareja@uc.cl>
2024-01-07 19:10:57 -0300

    binary and ternary search implemented.

---
+++ search.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <ctime>
+
+int binary_search()
+{
+    int tries = 0;
+    int guess;
+    int number = rand() % 100 + 1;
+    int diference = 50;
+    guess = 50;
+    std::cout << "current number = " << number << '\n';
+
+    do
+    {
+        std::cout << "current guess = " << guess << '\n';
+        if (guess > number)
+        {
+            guess = guess - diference / 2;
+            diference = diference / 2;
+            tries += 1;
+        }
+        else if (guess < number)
+        {
+            guess = guess + diference / 2;
+            diference = diference / 2;
+            tries += 1;
+        }
+        else
+        {
+            return tries + 1;
+        }
+    } while (guess != number);
+
+    return tries + 1;
+}
+
+int ternary_search()
+{
+    int tries = 0;
+    int guess;
+    int number = rand() % 100 + 1;
+    int low = 0;
+    int high = 100;
+
+    std::cout << "current number = " << number << '\n';
+
+    do
+    {
+        guess = (high - low) / 3 + low + 1;
+        std::cout << "current guess = " << guess << '\n';
+        std::cout << "low = " << low << '\n';
+        std::cout << "high = " << high << '\n';
+        std::cout << "-----------------------\n";
+        if (guess > number)
+        {
+            high = guess;
+            tries += 1;
+        }
+        else if (guess < number)
+        {
+            low = guess;
+            tries += 1;
+        }
+        else
+        {
+            return tries + 1;
+        }
+    } while (guess != number && tries < 10);
+
+    return tries + 1;
+}
+
+int main()
+{
+    srand(time(NULL));
+    std::cout << ternary_search() << '\n';
+    return 0;
+}
