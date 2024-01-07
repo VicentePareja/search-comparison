620158d25e7abb4eabd3bdd51cd4f5fc5540ef98
Vicente Pareja <vicente.pareja@uc.cl>
2024-01-07 19:35:42 -0300

    generalized search implemented.

--- search.cpp
+++ search.cpp
@@ -1,58 +1,22 @@
 #include <iostream>
 #include <ctime>
 
-int binary_search()
+int search(int divisor, int low, int high, int number)
 {
     int tries = 0;
     int guess;
-    int number = rand() % 100 + 1;
-    int diference = 50;
-    guess = 50;
-    std::cout << "current number = " << number << '\n';
-
-    do
-    {
-        std::cout << "current guess = " << guess << '\n';
-        if (guess > number)
-        {
-            guess = guess - diference / 2;
-            diference = diference / 2;
-            tries += 1;
-        }
-        else if (guess < number)
-        {
-            guess = guess + diference / 2;
-            diference = diference / 2;
-            tries += 1;
-        }
-        else
-        {
-            return tries + 1;
-        }
-    } while (guess != number);
-
-    return tries + 1;
-}
-
-int ternary_search()
-{
-    int tries = 0;
-    int guess;
-    int number = rand() % 100 + 1;
-    int low = 0;
     int diff;
-    int high = 100;
 
-    std::cout << "current number = " << number << '\n';
+    // std::cout << "current number = " << number << '\n';
 
     do
     {
         diff = high - low;
-        guess = diff / 3 + low + (diff % 3 > 0 ? 1 : 0);
-        std::cout << "current guess = " << guess << '\n';
-        std::cout << "low = " << low << '\n';
-        std::cout << "high = " << high << '\n';
-        std::cout << "-----------------------\n";
+        guess = diff / divisor + low + (diff % divisor > 0 ? 1 : 0);
+        // std::cout << "current guess = " << guess << '\n';
+        // std::cout << "low = " << low << '\n';
+        // std::cout << "high = " << high << '\n';
+        // std::cout << "-----------------------\n";
         if (guess > number)
         {
             high = guess;
@@ -67,14 +31,26 @@ int ternary_search()
         {
             return tries + 1;
         }
-    } while (guess != number && tries < 10);
+    } while (guess != number);
 
     return tries + 1;
 }
 
 int main()
 {
-    srand(time(NULL));
-    std::cout << ternary_search() << '\n';
+    double total = 0;
+    int high = 10000;
+    for (int divisor = 2; divisor <= 100; divisor++)
+    {
+
+        for (int number = 1; number <= high; number++)
+        {
+            total += search(divisor, 0, high, number);
+        }
+
+        std::cout
+            << "The average was: " << total / high << "\n";
+    }
+
     return 0;
 }
